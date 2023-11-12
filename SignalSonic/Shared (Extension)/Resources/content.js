2a8bbb9e51be01f02d253dce3d6b8afb6ac69b40
Mike Fikes <mike@fikesfarm.com>
2023-11-12 11:12:08 -0500

    Initial implementation

--- SignalSonic/Shared (Extension)/Resources/content.js
+++ SignalSonic/Shared (Extension)/Resources/content.js
@@ -1,7 +1,12 @@
 browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
+    console.log("mine");
     console.log("Received response: ", response);
+    console.log(window.getSelection().toString());
 });
 
-browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    console.log("Received request: ", request);
+browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+    if (request.method === "getSelection") {
+        const selectedText = window.getSelection().toString();
+        sendResponse({data: selectedText});
+    }
 });
--- SignalSonic/Shared (Extension)/Resources/popup.js
+++ SignalSonic/Shared (Extension)/Resources/popup.js
@@ -1,1 +1,21 @@
-console.log("Hello World!", browser);
+document.addEventListener('DOMContentLoaded', function() {
+    // Query the active tab
+    browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        if (tabs[0]) {
+            // Send a message to the content script in the active tab
+            browser.tabs.sendMessage(tabs[0].id, {method: "getSelection"}).then(response => {
+                if(response && response.data){
+                    var m = new jscw();
+                    m.setWpm(20);
+                    m.setEff(20);
+                    m.setFreq(600);
+                    m.setText(response.data);
+                    m.renderPlayer('player', m);
+                    m.play();
+                }
+            }).catch(error => console.error('Error:', error));
+        }
+    });
+});
+
+
