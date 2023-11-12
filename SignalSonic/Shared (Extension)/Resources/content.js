69dec7d7caf936c96773994fa38d43a45e76258c
Mike Fikes <mike@fikesfarm.com>
2023-11-12 14:08:43 -0500

    Remove unneeded code

--- SignalSonic/Shared (Extension)/Resources/content.js
+++ SignalSonic/Shared (Extension)/Resources/content.js
@@ -1,9 +1,3 @@
-browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
-    console.log("mine");
-    console.log("Received response: ", response);
-    console.log(window.getSelection().toString());
-});
-
 browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.method === "getSelection") {
         const selectedText = window.getSelection().toString();
