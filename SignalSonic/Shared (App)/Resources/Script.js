546e4f040a82a4b23341a3c5ee4113516430e81e
Mike Fikes <mike@fikesfarm.com>
2023-11-18 13:28:50 -0500

    Eliminate code duplication in logic

--- SignalSonic/Shared (App)/Resources/Script.js
+++ SignalSonic/Shared (App)/Resources/Script.js
@@ -1,71 +1,11 @@
-var m = new jscw();
-m.setStartDelay(0.2);
-
 document.addEventListener('DOMContentLoaded', function() {
-    var wpmSlider = setupSlider('wpm', 20, m.setWpm.bind(m));
-    var effSlider = setupSlider('eff', 20, m.setEff.bind(m));
-    setupSlider('freq', 600, m.setFreq.bind(m));
-    setupSlider('volume', parseFloat(localStorage.getItem('jscwlib_vol')) * 100 || 100, m.setVolume.bind(m), true);
-
     function playInputText() {
         var textToPlay = document.getElementById('textInput').value;
         m.setText(textToPlay);
         m.play();
     }
     
-    // Attach event listener to the play button
     document.getElementById('playButton').addEventListener('click', playInputText);
-    
-    function setupSlider(settingKey, defaultValue, updateFunction, isPercentage = false) {
-        var initialValue = isPercentage ? defaultValue : (localStorage.getItem(settingKey) || defaultValue.toString());
-        updateFunction(isPercentage ? initialValue / 100 : initialValue);
-        var slider = document.getElementById(settingKey + 'Slider');
-        var valueLabel = document.getElementById(settingKey + 'Value');
-        slider.value = initialValue;
-        valueLabel.textContent = formatLabel(settingKey, initialValue);
-
-        slider.addEventListener('input', function() {
-            var value = isPercentage ? parseInt(this.value, 10) / 100 : parseInt(this.value, 10);
-            localStorage.setItem(settingKey, value.toString());
-            valueLabel.textContent = formatLabel(settingKey, isPercentage ? this.value : value);
-            updateFunction(value);
-
-            // Ensure WPM is always >= EFF
-            if (settingKey === 'wpm' && value < parseInt(effSlider.value, 10)) {
-                effSlider.value = value;
-                localStorage.setItem('eff', value);
-                document.getElementById('effValue').textContent = formatLabel('fwpm', value);
-                m.setEff(value);
-            } else if (settingKey === 'eff' && value > parseInt(wpmSlider.value, 10)) {
-                wpmSlider.value = value;
-                localStorage.setItem('wpm', value);
-                document.getElementById('wpmValue').textContent = formatLabel('wpm', value);
-                m.setWpm(value);
-            }
-
-            restartPlayback();
-        });
-
-        return slider;
-    }
-
-    function formatLabel(settingKey, value) {
-        if (settingKey === 'volume') {
-            return Math.round(value) + '%';
-        } else if (settingKey === 'freq') {
-            return value + ' Hz';
-        } else if (settingKey === 'eff') {
-            return value + ' FWPM';
-        } else {
-            return value + ' ' + settingKey.toUpperCase();
-        }
-    }
-
-
-    function restartPlayback() {
-        m.stop();
-        m.play();
-    }
 });
 
 function show(platform, enabled, useSettingsInsteadOfPreferences) {
--- SignalSonic/Shared (Extension)/Resources/popup.js
+++ SignalSonic/Shared (Extension)/Resources/popup.js
@@ -1,11 +1,4 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var m = new jscw();
-    m.setStartDelay(0.2);
-    var wpmSlider = setupSlider('wpm', 20, m.setWpm.bind(m));
-    var effSlider = setupSlider('eff', 20, m.setEff.bind(m));
-    setupSlider('freq', 600, m.setFreq.bind(m));
-    setupSlider('volume', parseFloat(localStorage.getItem('jscwlib_vol')) * 100 || 100, m.setVolume.bind(m), true);
-
     // Query the active tab
     browser.tabs.query({active: true, currentWindow: true}, function(tabs) {
         if (tabs[0]) {
@@ -19,55 +12,5 @@ document.addEventListener('DOMContentLoaded', function() {
             }).catch(error => console.error('Error:', error));
         }
     });
-
-    function setupSlider(settingKey, defaultValue, updateFunction, isPercentage = false) {
-        var initialValue = isPercentage ? defaultValue : (localStorage.getItem(settingKey) || defaultValue.toString());
-        updateFunction(isPercentage ? initialValue / 100 : initialValue);
-        var slider = document.getElementById(settingKey + 'Slider');
-        var valueLabel = document.getElementById(settingKey + 'Value');
-        slider.value = initialValue;
-        valueLabel.textContent = formatLabel(settingKey, initialValue);
-
-        slider.addEventListener('input', function() {
-            var value = isPercentage ? parseInt(this.value, 10) / 100 : parseInt(this.value, 10);
-            localStorage.setItem(settingKey, value.toString());
-            valueLabel.textContent = formatLabel(settingKey, isPercentage ? this.value : value);
-            updateFunction(value);
-
-            // Ensure WPM is always >= EFF
-            if (settingKey === 'wpm' && value < parseInt(effSlider.value, 10)) {
-                effSlider.value = value;
-                localStorage.setItem('eff', value);
-                document.getElementById('effValue').textContent = formatLabel('fwpm', value);
-                m.setEff(value);
-            } else if (settingKey === 'eff' && value > parseInt(wpmSlider.value, 10)) {
-                wpmSlider.value = value;
-                localStorage.setItem('wpm', value);
-                document.getElementById('wpmValue').textContent = formatLabel('wpm', value);
-                m.setWpm(value);
-            }
-
-            restartPlayback();
-        });
-
-        return slider;
-    }
-
-    function formatLabel(settingKey, value) {
-        if (settingKey === 'volume') {
-            return Math.round(value) + '%';
-        } else if (settingKey === 'freq') {
-            return value + ' Hz';
-        } else if (settingKey === 'eff') {
-            return value + ' FWPM';
-        } else {
-            return value + ' ' + settingKey.toUpperCase();
-        }
-    }
-
-
-    function restartPlayback() {
-        m.stop();
-        m.play();
-    }
 });
+
